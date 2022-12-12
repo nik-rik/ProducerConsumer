52fe3ce43a6df2f188bc9e712be86ef30bbc8042
Nikhil Rikhye <nikhil.rikhye22@imperial.ac.uk>
2022-12-12 08:43:46 +0000

    First submission

--- main.cc
+++ main.cc
@@ -4,47 +4,161 @@
  ******************************************************************/
 
 #include "helper.h"
+#include <iostream>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <queue>
+
+//job class 
+class Job{
+public:
+	int id;
+	int duration;
+	Job(int id, int duration){
+		this->id = id;
+		this->duration = duration;
+	}
+};
+
+//global variables
+int q_size;
+int numProducers;
+int numConsumers;
+int n_jobs;
+
+
+//semaphores
+sem_t emptySem;
+sem_t full;
+sem_t mutex;
+
+
+//queue
+queue<Job*> jobQueue;
 
 void *producer (void *id);
 void *consumer (void *id);
 
 int main (int argc, char **argv)
 {
+  if (argc !=5){
+    
+    cerr << "Incorrect number of arguments!" << endl;
+    return 0;
 
-  // TODO
+  }
 
-  pthread_t producerid;
-  int parameter = 5;
+  //initialise variables
+  q_size = atoi(argv[1]);
+  n_jobs = atoi(argv[2]);
+  numProducers = atoi(argv[3]);
+  numConsumers = atoi(argv[4]);
 
-  pthread_create (&producerid, NULL, producer, (void *) &parameter);
+  //initialise semaphores
+  sem_init(&emptySem, 0, q_size);
+  sem_init(&full, 0, 0);
+  sem_init(&mutex, 0, 1);
 
-  pthread_join (producerid, NULL);
+  //initialise threads
+  pthread_t producerid[numProducers];
+  pthread_t consumerid[numConsumers];
 
-  cout << "Doing some work after the join" << endl;
+  //create producer threads
+  for(int i = 0; i < numProducers; i++){
+    int* id = new int;
+    *id = i;
+    pthread_create(&producerid[i], NULL, producer, (void*)id);
+  }
 
+  //create consumer threads
+  for(int i = 0; i < numConsumers; i++){
+    int* id = new int;
+    *id = i;
+    pthread_create(&consumerid[i], NULL, consumer, (void*)id);
+  }
+  
+    //join threads
+  for(int i = 0; i < numProducers; i++){
+    pthread_join(producerid[i], NULL);
+  }
+  for(int i = 0; i < numConsumers; i++){
+    pthread_join(consumerid[i], NULL);
+  }
+    
+  //destroy semaphores
+  sem_destroy(&emptySem);
+  sem_destroy(&full);
+  sem_destroy(&mutex);
+  
   return 0;
+
 }
 
 void *producer (void *parameter) 
 {
+  int id = *((int*)parameter);
+  int jobsProduced = 0;
+  while(jobsProduced < n_jobs){
+    //produce job
+    int jobId = rand() % 100;
+    int jobDuration = rand() % 10 + 1;
+    Job* newJob = new Job(jobId, jobDuration);
+    
+    //wait for empty slot
+    sem_wait(&emptySem);
+    sem_wait(&mutex);
+		
+    //add job to queue
+    jobQueue.push(newJob);
+    cout << "Producer(" << id << "): Job id " << jobId << " duration " << jobDuration << endl;
+    
+    //signal full and mutex
+    sem_post(&mutex);
+    sem_post(&full);
+    
+    //sleep for 1-5 seconds
+    sleep(rand() % 5 + 1);
 
-  // TODO
-
-  int *param = (int *) parameter;
-
-  cout << "Parameter = " << *param << endl;
-
-  sleep (5);
-
-  cout << "\nThat was a good sleep - thank you \n" << endl;
-
+    jobsProduced++;
+  }
+  
+  cout << "Producer(" << id << "): No more jobs to generate." << endl;
   pthread_exit(0);
+  
 }
 
-void *consumer (void *id) 
+
+
+
+
+void *consumer (void *parameter) 
 {
-    // TODO 
+  int id = *((int*)parameter);
+  int jobsConsumed = 0;
+  while(jobsConsumed < n_jobs){
+    //wait for full slot
+    sem_wait(&full);
+    sem_wait(&mutex);
+		
+    //get job from queue
+    Job* job = jobQueue.front();
+    jobQueue.pop();
+    cout << "Consumer(" << id << "): Job id " << job->id << " executing sleep duration " << job->duration << endl;
+		
+    //signal empty and mutex
+    sem_post(&mutex);
+    sem_post(&emptySem);
+		
+    //sleep for job duration
+    sleep(job->duration);
 
-  pthread_exit (0);
-
+    jobsConsumed++;
+  }
+  
+  cout << "Consumer(" << id << "): No more jobs left" << endl;
+  pthread_exit(0);
+	
 }
